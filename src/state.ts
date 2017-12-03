96dc239bfdceaf11cdcfa9015007f9143e616538
llyb120 <llyb120@163.com>
2017-12-03 13:27:03 +0800

    pub

---
+++ src/state.ts
@@ -0,0 +1,7 @@
+
+export class State{
+    public name;
+    public condition : Function[] = [];
+    public from :string[] = [];
+    public to :string[] = [];
+}
--- src/state_machine.ts
+++ src/state_machine.ts
@@ -1,22 +1,72 @@
+import { State } from './state';
 export class StateMachine{
-    when(){
 
+    private currentFactory: State = null;
+    private _state:string = "";
+    private stateMap:{
+        [key:string] : State
+    } = {};
+
+
+    when(condition: Function)
+    when(conditions: Function[])
+    when(condition: Function | Function[]){
+        
     }   
 
     add(){
-
+        if(this.currentFactory === null){
+            throw new Error();
+        }
+        this.stateMap[this.currentFactory.name] = this.currentFactory;
+        this.currentFactory = null;
     }
 
-    from(){
-
+    from(state:string);
+    from(states:string[])
+    from(stateOrStates:string | string[]){
+        let states = [];
+        if(typeof stateOrStates !== 'string'){
+            states = stateOrStates;
+        }
+        else{
+            states.push(stateOrStates);
+        }
+        if(this.currentFactory === null){
+            this.currentFactory = new State;
+        }
+        this.currentFactory.from = states;
     }
 
-    to(){
-
+    to(state:string)
+    to(states:string[])
+    to(stateOrStates : string | string[]){
+        let states = [];
+        if(typeof stateOrStates !== 'string'){
+            states = stateOrStates;
+        }
+        else{
+            states.push(stateOrStates);
+        }
+        if(this.currentFactory === null){
+            this.currentFactory = new State;
+        }
+        this.currentFactory.to = states;
     }
 
-    state(){
-        
+    name(name : string){
+        if(this.currentFactory === null){
+            this.currentFactory = new State;
+        } 
+        this.currentFactory.name = name;
+    }
+
+    get state(){
+        return this._state;
+    }
+
+    set state(state){
+
     }
 
 }
